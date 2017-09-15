cbb79b80e48b356ea2f90b73c773b7606ca881b9
linlongqing <longqing.lin@krvision.com>
2017-09-15 17:32:50 +0800

    create files

---
+++ cmake-build-release/CMakeFiles/caffeclassification.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/caffeclassification.dir/main.cpp.o"
+  "caffeclassification.pdb"
+  "caffeclassification"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/caffeclassification.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
